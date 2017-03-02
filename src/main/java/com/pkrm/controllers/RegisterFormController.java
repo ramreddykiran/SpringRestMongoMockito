abcf5434ddcd53022f72cf86d4e5a632a8c2fba4
ramreddykiran <ramreddykiran@gmail.com>
2017-03-02 17:22:13 +0530

    Added Junit tests by calling API in controller test class.

--- src/main/java/com/pkrm/controllers/RegisterFormController.java
+++ src/main/java/com/pkrm/controllers/RegisterFormController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +28,7 @@ public class RegisterFormController {
 	@Setter
 	private RegisterFormService registerFormservice;
 	
-	 @RequestMapping(value="/form-submit", method = RequestMethod.POST)
+	 @RequestMapping(value="/save-employee", method = RequestMethod.POST)
 	 @ResponseBody
 	    public ResponseEntity<?> saveEmployee(@RequestBody User emp) throws Exception { try{
 			 registerFormservice.saveUser(emp);
@@ -58,7 +59,8 @@ public class RegisterFormController {
 	 
 	 @RequestMapping(value="/save", method = RequestMethod.POST)
 	 @ResponseBody
-	    public ResponseEntity<?> save(@RequestBody Example ex) throws Exception { try{
+	    public ResponseEntity<?> save(@RequestBody Example ex) throws Exception {
+		 try{
 			 registerFormservice.saveEx(ex);
 			 return new ResponseEntity<String>("success",HttpStatus.OK);
 		 } catch(Exception e) {
@@ -66,10 +68,16 @@ public class RegisterFormController {
 		 }
 	    }
 	 
-	 @RequestMapping(value="/retrieve", method = RequestMethod.GET)
-	    public ResponseEntity<?> get(@RequestParam(value = "dept-id", required = true) String deptId) throws Exception { try{
+	/* @RequestMapping(value="/retrieve", method = RequestMethod.GET)
+	   public ResponseEntity<?> get(@RequestParam(value = "dept-id", required = true) String deptId) throws Exception { try{*/
+	 @RequestMapping(value="/retrieve/{dept-id}", method = RequestMethod.GET)
+	 public ResponseEntity<?> get(@PathVariable(value="dept-id") String deptId) throws Exception { try{
 	    	Example ex = registerFormservice.getEmp(deptId);
-			 return new ResponseEntity<String>("success",HttpStatus.OK);
+	    	if(null == ex) {
+	    		return new ResponseEntity<String>("Employee not found ",HttpStatus.NOT_FOUND);
+	    	} else {
+	    		return new ResponseEntity<Example>(ex, HttpStatus.OK);
+	    	}
 		 } catch(Exception e) {
 			 return new ResponseEntity<String>("falure",HttpStatus.INTERNAL_SERVER_ERROR);
 		 }
--- src/test/java/com/pkrm/controllers/RegisterFormControllerTest.java
+++ src/test/java/com/pkrm/controllers/RegisterFormControllerTest.java
@@ -2,33 +2,62 @@ package com.pkrm.controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Matchers.anyString;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.google.gson.Gson;
 import com.pkrm.builders.UserBuilder;
+import com.pkrm.domain.Example;
 import com.pkrm.domain.User;
 import com.pkrm.service.RegisterFormService;
 import com.pkrm.util.UserUtils;
 
+import static org.mockito.Mockito.verify;
+
+/**
+ * 
+ * @author kramreddy
+ *This test class contains test cases 
+ *by calling API for both GET(with request params) and POST (passing request body) operations
+ *and control methods
+ *included static methods mocking, verifying the results with ArgumentCaptor
+ */
 @PrepareForTest(UserUtils.class)
 @RunWith(PowerMockRunner.class)
 public class RegisterFormControllerTest {
+	
+	private MockMvc mockMvc;
+	
 	@InjectMocks
 	private RegisterFormController controller;
 	
 	@Mock
 	private RegisterFormService service;
 	
+	@Before
+	public void init() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller)
+					.build();
+	}
 	
 	@Test
 	public void shouldReceiveUserDetails() {
@@ -42,6 +71,59 @@ public class RegisterFormControllerTest {
 		
 		assertSame(user, response.getBody());
 		assertEquals(HttpStatus.OK,  response.getStatusCode());
+	}
+	
+	@Test
+	public void shouldSaveUserDetails() throws Exception {
+		Gson gson = new Gson();
+		String userJson = gson.toJson(new UserBuilder().build());
+		mockMvc.perform(post("/save-employee")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(userJson))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldGetHttpResponseNotFound_WhenUserDetailsAreNotPresent() throws Exception{
+		mockMvc.perform(get("/user-details")
+						.param("user-id", "12321"))
+						.andExpect(status().isNotFound())
+						.andReturn().getResponse();
+		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+		verify(service).retrieveUserDetails(captor.capture());
+		assertEquals("12321", captor.getValue());
+	}
+	
+	@Test
+	public void shouldGetUserDetailsForUserId() throws Exception{
+		User user = new UserBuilder().build();
+		when(service.retrieveUserDetails(anyString())).thenReturn(user);
+		MockHttpServletResponse response = mockMvc.perform(get("/user-details")
+						.param("user-id", "12321"))
+						.andExpect(status().isOk())
+						.andReturn().getResponse();
+		Gson gson = new Gson();
+		assertEquals(gson.toJson(user), response.getContentAsString());
+	}
+	
+	@Test
+	public void sholdReturnHttpStatusNotFoundWhenExampleNotFound() throws Exception {
+		mockMvc.perform(get("/retrieve/dept1"))
+				.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void shouldReturnExampleDetails() throws Exception{
+		Example example = new Example();
+		when(service.getEmp(anyString())).thenReturn(example);
+		MockHttpServletResponse response = mockMvc.perform(get("/retrieve/department1"))
+						.andExpect(status().isOk())
+						.andReturn().getResponse();
+		Gson gson = new Gson();
+		Example resposneEx = gson.fromJson(response.getContentAsString(), Example.class);
+		
+		assertEquals(example.getDbl(), resposneEx.getDbl());
+		
 	}
 	
 }
